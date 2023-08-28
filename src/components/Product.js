0581870d27b0fd7b9a1c6b226c78a191ff2441fd
WanjikuN <cikunjoroge4@gmail.com>
2023-08-28 14:49:06 +0300

    feat: add products

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,17 @@ export default function Product({name,brand, gender,category,price,stock,url}){
    
     return(
         <div id="product">
+
            <img src={url} alt={name}/> 
-       
+            <div id="row2">
+                <p id="brand">
+                    {brand} | {gender.toLowerCase()}
+                </p>
+                <p>
+                    ${price}
+                </p>
+            </div>
+            <h3>{name}</h3>
         </div>
     )
 }
