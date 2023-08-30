16e5ec555ac02de78567ecbc2195dec4a14173cb
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-30 14:55:40 +0300

    update

--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -3,8 +3,29 @@ import './LoginSignup.css'
 
 
 const  LoginSignup = () => {
- 
   const [action, setAction] = useState("Sign Up");
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const addUser = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method : 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+      },
+     body: JSON.stringify({ name, email, password})
+      });
+
+
+        const data = await response.json();
+        console.log('User added:', data);
+    } catch (err) {
+      console.error('Error adding user:', err);
+    }
+  };
 
   return (
     <div className='Container'>
@@ -14,29 +35,29 @@ const  LoginSignup = () => {
         </div>
           <div className="inputs">
             {action==="Login"?<div></div>: <div className="input">
-          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
         </div>}
          
         </div>
 
           <div className="inputs">
           <div className="input">
-          <input type="email" placeholder="Email"/>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
         </div>
 
           <div className="inputs">
           <div className="input">
-          <input type="password" placeholder="Password"/>
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
         </div>
-          {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgotten password <span>Click Here!</span> </div>}
+          {action==="Sign Up"? null : (<div className="forgot-password">Forgotten password <span>Click Here!</span> </div>)}
           <div className="Submit-container">
-          <div className={action==="Login"?"Submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-          <div className={action==="Sign Up"?"Submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+          <div className={action==="Login"?"Submit grey":"submit"}onClick={()=>{setAction("Sign Up"); addUser();}}>Sign Up</div>
+          <div className={action==="Sign Up"?"Submit grey":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
        </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginSignup;
