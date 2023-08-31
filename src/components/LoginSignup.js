bf726e866666c74fac4db5aec6fddcc6aab65b2f
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-31 15:14:54 +0300

    Login-Signup style and functionality update

--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import './LoginSignup.css'
-
+import { useHistory } from 'react-router-dom'
 
 const  LoginSignup = () => {
   const [action, setAction] = useState("Sign Up");
@@ -23,21 +23,24 @@ const  LoginSignup = () => {
         console.log('User added:', data);
       if (action === 'Login') {
         const response = await fetch('http://localhost:3030/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),
-        });
+          method: 'GET',
+      });
 
-        const data = await response.json();
-        if (data.success) {
+      const users = await response.json();
+      const user = users.find(user => user.email === email && user.password === password);
+   
+        if (user) {
           console.log('Login successful');
           
         } else {
           console.log('Login failed');
         }
       }
+
+      setName("");
+      setEmail("");
+      setPassword("");
+
     } catch (err) {
       console.error('Error:', err);
     }
@@ -67,7 +70,7 @@ const  LoginSignup = () => {
         </div>
         </div>
           <div className="Submit-container">
-          <div className={action==="Sign Up" ? "Submit" : "submit-active"}onClick={()=>{if(action === "Sign Up") handleSubmit();}}>Sign Up</div>
+          <div className={action==="Sign Up" ? "Submit" : "submit-active"}onClick={()=>{if(action === "Sign Up") {handleSubmit();} setAction('Sign Up')}} >Sign Up</div>
           <div className={action==="Login" ? "Submit" : "submit-inactive"}onClick={()=>{if(action === "Login") {handleSubmit();} setAction('Login') }} >Login</div>
        </div>  
     </div>
