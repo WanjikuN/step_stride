94bc99a5f546a29cb0bc30d390ffe37cb3ab7df0
Sammybett-bit <bettsammy20@gmail.com>
2023-08-31 08:12:23 +0300

    checkout.js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 30px 30px;
-  border-radius:4px;
+  border-radius: 4px;
 `;
 
 const Logo = styled.h1`
@@ -49,7 +49,7 @@ const LinksContainer = styled.div`
   display: flex;
 `;
 
-function Navbar({cartLength}) {
+function Navbar({ cartLength }) {
   return (
     <Nav>
       <Logo>
@@ -69,9 +69,15 @@ function Navbar({cartLength}) {
           <ListItem>
             <NavLink to="/login">Login</NavLink>
           </ListItem>
+          <ListItem>
+            <NavLink to="/checkout">Checkout</NavLink> 
+          </ListItem>
         </List>
       </LinksContainer>
-      <NavLink to="/cart" className="length" > <CartIcon id="ca"  icon={faShoppingCart} size="lg" /><p id="count">{cartLength}</p></NavLink>
+      <NavLink to="/cart" className="length">
+        <CartIcon id="ca" icon={faShoppingCart} size="lg" />
+        <p id="count">{cartLength}</p>
+      </NavLink>
     </Nav>
   );
 }
