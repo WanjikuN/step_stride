360b6147d67fda31254d4b888117e251eab5a363
WanjikuN <cikunjoroge4@gmail.com>
2023-09-03 23:17:44 +0300

    Add a loading feature

--- src/components/Products.js
+++ src/components/Products.js
@@ -34,7 +34,9 @@ export default function Products(){
         setSelectedBrand(e.target.value);
     }
     const sortedBrand = selectedBrand ==="All"? sortedGender: sortedGender.filter(s=>s.brand.toLowerCase() === selectedBrand.toLowerCase());
-    
+    if (!sneakers.length) {
+        return <h2>Loading...</h2>;
+    }
     return (
         <div id ="products-cont">
         <Filter handleName={handleName} handleGender={handleGender} handleBrand={handleBrand} selectedBrand={selectedBrand}/>
